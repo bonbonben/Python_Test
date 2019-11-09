d82bb0916494a5b041959dd7810954d713afa4dd
Ben Hung <hben8352@gmail.com>
2019-11-09 00:07:08 -0500

    make sure all the nodes have edge

--- random-network.py
+++ random-network.py
@@ -7,8 +7,8 @@
 import networkx as ne
 import matplotlib.pyplot as mp
 import random
-import numpy as np
 from random import choice
+import numpy as np
 import pandas as pd
 #erdos renyi graphy
 
@@ -38,7 +38,7 @@ def count(Belief_Array):
             count3+=1
     return count1,count2,count3
 
-for t in range(1,11):                   # number of experiments is 10
+for t in range(1,11):                    # number of experiments is 10
     print ("No.", t,"experiment:")
     x = random.randrange(3,10,1)        # number of agents is 3~10
     y = random.randrange(5,8,1)/10      # line possibility to connect other agents is 0.5~0.8
@@ -52,40 +52,51 @@ for t in range(1,11):                   # number of experiments is 10
     mp.show()
     p = []
     temp = 0
-
+    Belief_Array = []
+    check = 0                           # make sure all the nodes have edge
+    
     for i in range(0,x):
-        print ('degree of node', i,'is', rg.degree(i))
-        adjacent = []
-        for j in rg.neighbors(i):
-            adjacent.append(j)
-        print ('adjacent nodes of node', i,'is', adjacent)
-        p.append(adjacent)
-        temp += rg.degree(i)
+        if rg.degree(i) == 0:
+            print ('degree of node', i,'is 0')
+            adjacent = []
+            print ('adjacent nodes of node', i,'is', adjacent)
+            p.append(adjacent)
+            Belief_Array.append((random.randrange(0,3,1)) / 2)
+            check = 1                   # invalid network
+        else:
+            print ('degree of node', i,'is', rg.degree(i))
+            adjacent = []
+            for j in rg.neighbors(i):
+                adjacent.append(j)
+            print ('adjacent nodes of node', i,'is', adjacent)
+            p.append(adjacent)
+            temp += rg.degree(i)
+            Belief_Array.append((random.randrange(0,3,1)) / 2)
 
     edge = int(temp/2)
     print ('\nnumber of edges:', edge, '\n')
 
     Martix = np.array([[0, 0, 0.5], [0, 0.5, 1], [0.5, 1, 1]])
-
-    Belief_Array = []
-    for work in range(0,x):
-        Belief_Array.append((random.randrange(0,3,1)) / 2)
-
+    
     print("Original Belief_Array:",Belief_Array)
+    print(p, '\n')
 
-    for r in range(1,1000):
-        A = random.randrange(0,len(p),1)
-        B = random.choice(p[A])
-        a = Martix[int(2*Belief_Array[A]),int(2*Belief_Array[B])]
-        Belief_Array[A]=a
-        Belief_Array[B]=a
-        converge=result(r,Belief_Array)
-        if converge == 1:
-            print("Converge at 0\n")
-            break
-        elif converge == 2:
-            print("Converge at 1\n")
-            break
-        else:
-            pass
+    if check == 0:
+        for r in range(1,1000):
+            A = random.randrange(0,len(p),1)
+            B = random.choice(p[A])
+            a = Martix[int(2*Belief_Array[A]),int(2*Belief_Array[B])]
+            Belief_Array[A]=a
+            Belief_Array[B]=a
+            converge=result(r,Belief_Array)
+            if converge == 1:
+                print("Converge at 0\n")
+                break
+            elif converge == 2:
+                print("Converge at 1\n")
+                break
+            else:
+                pass
+    else:
+        print ('invalid network\n')
 
